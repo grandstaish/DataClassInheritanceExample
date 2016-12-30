4fe680302cd175ea1f0c08677a01556cca504255
Bradley Campbell <grandstaish@gmail.com>
2016-12-30 22:17:00 +1300

    Add parcelable code

--- app/src/main/java/com/example/bradleycampbell/dataclassinheritanceexample/MyModel.kt
+++ app/src/main/java/com/example/bradleycampbell/dataclassinheritanceexample/MyModel.kt
@@ -1,9 +1,19 @@
 package com.example.bradleycampbell.dataclassinheritanceexample
 
+import android.os.Parcel
+import android.os.Parcelable
 import paperparcel.PaperParcel
 
 @PaperParcel
-data class MyModel(val something: Int) : MyModelBase(something)
+data class MyModel(val something: Int) : MyModelBase(something), Parcelable {
+  override fun writeToParcel(dest: Parcel?, flags: Int) {
+    throw UnsupportedOperationException()
+  }
+
+  override fun describeContents(): Int {
+    throw UnsupportedOperationException()
+  }
+}
 
 // This code will not work with PaperParcel, which is fine, but there's no error message being
 // shown in kapt to inform the user what is wrong. It just says:
